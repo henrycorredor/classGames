6f20a82c06fecebb8c7e5bd3844f3bbb2f73d9b1
Henry Corredor <feijizoule@126.com>
2021-12-20 20:03:47 +0800

    eliminados directorios anteriores del repo

--- server/gamesLibrary/index.js
+++
@@ -1,7 +0,0 @@
-const fastCardsClass = require('./fastCardsClass')
-const memorySnakeClass = require('./memorySnakeClass')
-
-module.exports = {
-	'fastCards': fastCardsClass,
-	'memorySnake': memorySnakeClass
-}
